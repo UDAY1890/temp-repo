fe40619e2fc2f08568ffa9b8863df13f0c63df2c
Jaydip614 <jaydipbaraiya614@gmail.com>
2025-04-11 09:19:31 +0530

    Initial commit

---
+++ Server.java
@@ -0,0 +1,36 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) {
+        try{
+            ServerSocket serverSocket = new ServerSocket(5050); //just accepting request don't process it
+            Socket socket = serverSocket.accept(); //for process request and also used to send data
+
+            while (true) {
+                Scanner sc = new Scanner(System.in);
+                DataInputStream din = new DataInputStream(socket.getInputStream());
+                String data = din.readUTF();
+                System.out.println(data);
+
+                if(data.trim().endsWith("bye")) {
+                    break;
+                }
+    
+                DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+                System.out.print("Server: ");
+                String message = sc.nextLine();
+                dout.writeUTF("Server : " + message);
+
+                sc.close();
+            }
+            serverSocket.close();
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
